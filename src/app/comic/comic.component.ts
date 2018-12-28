b8c9faa9f6df72313a1076abc8626c49ea070e1f
barath <barath@commutatus.com>
2018-12-28 13:50:24 +0530

    repeating numbers stoped

--- src/app/comic/comic.component.ts
+++ src/app/comic/comic.component.ts
@@ -11,22 +11,48 @@ export class ComicComponent implements OnInit {
   comicData: any;
   alert: boolean = true;
   loader: boolean = false;
+  randomNumArr = [];
+  maximumComicLimit = 3;
   constructor(private http: HttpClient) { }
 
   ngOnInit() { }
 
   generateComic() {
     this.loader = true;
-    this.randomNumber = Math.floor(Math.random() * 1000);
+    this.randomNumber = Math.floor(Math.random() * this.maximumComicLimit) + 1;
+    if (this.randomNumArr.length >= this.maximumComicLimit) {
+      alert('fuck off');
+      return;
+    }
+    if(this.randomNumArr.length && this.doesNumberAlreadyExist(this.randomNumber)) {
+      this.generateComic();
+      return;
+    }
+    this.randomNumArr.push(this.randomNumber);
+    this.apiFetch();
+    console.log("comes here else", this.randomNumArr);
+  }
+
+  apiFetch() {
     this.http.get(`https://xkcd.now.sh/${this.randomNumber}`)
     .subscribe((response) => {
       this.alert = false;
       this.loader = false;
       this.comicData = response;
-      console.log(response);
     }, err => {
         console.log(err);
         this.loader = true;
     });
+  }
+
+  doesNumberAlreadyExist(num) {
+    console.log('coming number', num);
+
+    if(this.randomNumArr.includes(num)) {
+        return true;
+    }
+    else {
+      return false;
+    }
   }
 }
